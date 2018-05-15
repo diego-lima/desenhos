db436fec9b89781024f987d3a1d5abcb74f4edd8
beatriz-soares <bs.souza1998@gmail.com>
2018-05-15 19:29:52 -0300

    main pra circulo

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
 #include "headers/tela.h"
-#include "headers/reta.h"
+#include "headers/circulo.h"
 using namespace std;
 
 int main()
 {
-    Tela t(10,10,'a');
-    t.pincel('-');
-    Reta r(0,0,5,5);
-    r.draw(t);
+    Tela t(10,10,'-');
+    t.pincel('*');
+    Circulo r(5,5,2);
+    r.draw(t,1);
 
 //    t.pincelar(1,1);
     cout<<t;
