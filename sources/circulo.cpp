cccdd1ff25bb793d9f372cdc1cab04a8365c98c4
beatriz-soares <bs.souza1998@gmail.com>
2018-05-15 19:28:30 -0300

    bresenham de círculo

--- sources/circulo.cpp
+++ sources/circulo.cpp
@@ -1,4 +1,6 @@
 #include "headers/circulo.h"
+#include <iostream>
+using namespace std;
 
 Circulo::Circulo(int xc, int yc, int r)
 {
@@ -6,6 +8,46 @@ Circulo::Circulo(int xc, int yc, int r)
     y_centro = yc;
     raio = r;
 }
+void drawCircle(int x, int y, int xc, int yc, Tela &t, bool fillmode)
+{
+    if (fillmode){
+        for (int i=-y; i<=y; i++){
+            t.pincelar(x+xc,i+yc);
+            t.pincelar(-x+xc,i+yc);
+        }
+        for (int i=-x; i<=x; i++){
+            t.pincelar(y+xc, i+yc);
+            t.pincelar(-y+xc, i+yc);
+        }
+    }
+    else{
+        t.pincelar(x+xc,y+yc);
+        t.pincelar(x+xc, -y+yc);
+        t.pincelar(-x+xc,y+yc);
+        t.pincelar(-x+xc, -y+yc);
+        t.pincelar(y+xc, x+yc);
+        t.pincelar(y+xc, -x+yc);
+        t.pincelar(-y+xc, x+yc);
+        t.pincelar(-y+xc, -x+yc);
+    }
+}
 void Circulo::draw(Tela &t, bool fillmode){
+    int r=raio, xc=x_centro, yc=y_centro, pk, x, y;
+    pk = 3 - 2*r;
+    x=0; y = r;
+    drawCircle(x,y,xc,yc, t, fillmode);
+    while(x < y)
+    {
+        if(pk <= 0)
+        {
+            pk = pk + (4*x) + 6;
+            drawCircle(++x,y,xc,yc, t, fillmode);
+        }
+        else
+        {
+            pk = pk + (4*(x-y)) + 10;
+            drawCircle(++x,--y,xc,yc, t, fillmode);
+        }
+    }
 
 }
--- sources/reta.cpp
+++ sources/reta.cpp
@@ -10,60 +10,47 @@ Reta::Reta(int _xi, int _yi, int _xf, int _yf)
     y_fim = _yf;
 }
 void Reta::draw(Tela &t, bool fillmode){
-   int x1=x_ini, x2=x_fim, y1=y_ini, y2=y_fim, Tamanho, i=0;
-//   int x = x1, y = y1, Delta_x, Delta_y, s1, s2, Temp, Troca, new_e;
-//    Delta_x = abs(x2 - x1);
-//    Delta_y = abs(y2 - y1);
-//    s1=(x2 - x1);
-//    s2=(y2 - y1);
-//    if(Delta_y > Delta_x){
-//      Temp = Delta_x;
-//      Delta_x = Delta_y;
-//      Delta_y = Temp;
-//      Troca = 1;
-//    }
-//    else{
-//      Troca = 0;
-//    }
-//    new_e = 2*Delta_y - Delta_x;
-//    for(i=1; i<= Delta_x; i++){
-//        cout<<i<<endl;
-//      t.pincelar(x,y);
-//      while (new_e >= 0){
-//        if(Troca == 1){
-//          //Muda para a proxima linha de rasterização
-//          x = x + s1;
-//        }
-//        else{
-//          y = y + s2;
-//        }
-//        new_e = new_e - 2*Delta_x;
-//      }
+   int x1=x_ini, x2=x_fim, y1=y_ini, y2=y_fim;
 
-//      //Permanece nesta linha de rasterização
-//      if(Troca == 1){
-//        y = y + s2;
-//      }
-//      else{
-//        x = x + s1;
-//      }
-//      new_e= new_e + 2*Delta_y;
-//    }
-   float Delta_x, Delta_y, x=x_ini, y=y_ini;
-    // Tamanho: Aproxima o tamanho da reta
-     if( abs(x2 - x1) > abs(y2 - y1))
-          Tamanho = abs(x2 - x1);
-     else
-          Tamanho = abs(y2 - y1);
-     //seleciona o maior dos valores entre Delta_x e Delta_y como unidade de rasterização
-     Delta_x = float(x2 - x1)/Tamanho;
-     Delta_y = float(y2 - y1)/Tamanho;
-     i = 1;
-     while(i < Tamanho){
-     //round: valor arredondado de um dado número real. round(-8.6) = -9; round(-8.4) = -8
-       t.pincelar(round(x), round(y));
-       x = x + Delta_x;
-       y = y + Delta_y;
-       i = i + 1;
+           // Bresenham's line algorithm
+     const bool steep = (fabs(y2 - y1) > fabs(x2 - x1));
+     if(steep)
+     {
+       std::swap(x1, y1);
+       std::swap(x2, y2);
+     }
+
+     if(x1 > x2)
+     {
+       std::swap(x1, x2);
+       std::swap(y1, y2);
+     }
+
+     const float dx = x2 - x1;
+     const float dy = fabs(y2 - y1);
+
+     float error = dx / 2.0f;
+     const int ystep = (y1 < y2) ? 1 : -1;
+     int y = (int)y1;
+
+     const int maxX = (int)x2;
+
+     for(int x=(int)x1; x<maxX; x++)
+     {
+       if(steep)
+       {
+           t.pincelar(y,x);
+       }
+       else
+       {
+           t.pincelar(x,y);
+       }
+
+       error -= dy;
+       if(error < 0)
+       {
+           y += ystep;
+           error += dx;
+       }
      }
 }
