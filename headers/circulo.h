3c42f07ad67b0691988585e7c7ef96242a3eafce
Diego Lima <ddddiegolima@gmail.com>
2018-05-16 07:23:41 -0300

    Documenta circulo

--- headers/circulo.h
+++ headers/circulo.h
@@ -8,7 +8,18 @@ class Circulo : public FiguraGeometrica
 {
     int x_centro, y_centro, raio;
 public:
+    /**
+     * @brief Construtor da classe
+     * @param xc coordenada x do centro do circulo
+     * @param yc coordenada y do centro do circulo
+     * @param r raio do circulo
+     */
     Circulo(int xc, int yc, int r);
+    /**
+     * @brief desenha na tela
+     * @param t tela a ser desenhada
+     * @param fillmode se é pra preencer (1) ou nao
+     */
     void draw(Tela &t, bool fillmode=1);
 };
 
