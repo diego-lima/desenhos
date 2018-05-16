fb61b012bbc4392afffe28fa02ad5ca501b20dac
Diego Lima <ddddiegolima@gmail.com>
2018-05-16 07:28:00 -0300

    Update retangulo.h

--- headers/retangulo.h
+++ headers/retangulo.h
@@ -8,6 +8,13 @@ class Retangulo : public FiguraGeometrica
 {
     int x,y,altura,largura;
 public:
+    /**
+     * @brief construtor da classe
+     * @param _xi coordenada x do ponto inicial
+     * @param _yi coordenada y do ponto inicial
+     * @param alt altura
+     * @param larg largura
+     */
     Retangulo(int _xi, int _yi, int alt, int larg);
     void draw(Tela &t, bool fillmode=1);
 };
