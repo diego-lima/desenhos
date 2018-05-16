840a254f5a5fea9155d80ce298f17a71208ebb69
Diego Lima <ddddiegolima@gmail.com>
2018-05-16 07:26:00 -0300

    Documenta reta

--- headers/reta.h
+++ headers/reta.h
@@ -8,6 +8,14 @@ class Reta : public FiguraGeometrica
 {
     int x_ini, x_fim, y_ini, y_fim;
 public:
+    /**
+     * @brief construtor da classe
+     * @param _xi coordenada x do ponto inicial
+     * @param _yi coordenada y do ponto inicial
+     * @param _xf coordenada x do ponto final
+     * @param _yf coordenada y do ponto final
+     * @param fillmode se é pra preencer (1) ou nao
+     */
     Reta(int _xi, int _yi, int _xf, int _yf);
     void draw(Tela &t, bool fillmode=0);
 };
